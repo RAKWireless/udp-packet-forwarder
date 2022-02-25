bbb63618cc04bba6d7255cc722773cefc4a41446
Xose Pérez <xose.perez@gmail.com>
2022-02-25 14:42:40 +0100

    Using gpiod instead of /sys/class/gpio for concentrator reset

--- runner/reset.sh
+++ runner/reset.sh
@@ -1,37 +1,23 @@
 #!/usr/bin/env bash
 
-WAIT_GPIO() {
-    sleep 0.01
-}
-
 RESET_GPIO=${RESET_GPIO:-17}
 POWER_EN_GPIO=${POWER_EN_GPIO:-0}
 POWER_EN_LOGIC=${POWER_EN_LOGIC:-1}
 
+GPIOSET="gpioset -m time -u 100000 gpiochip0"
+
 # Enable gateway
 if [[ $POWER_EN_GPIO -ne 0 ]]; then
     echo "Concentrator enabled through GPIO$POWER_EN_GPIO"
-    if [[ -d /sys/class/gpio/gpio$POWER_EN_GPIO ]]; then
-        echo $POWER_EN_GPIO > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    echo $POWER_EN_GPIO > /sys/class/gpio/export; WAIT_GPIO
-    echo out > /sys/class/gpio/gpio$POWER_EN_GPIO/direction; WAIT_GPIO
-    echo $POWER_EN_LOGIC > /sys/class/gpio/gpio$POWER_EN_GPIO/value; WAIT_GPIO
-    echo $POWER_EN_GPIO > /sys/class/gpio/unexport; WAIT_GPIO
+    ${GPIOSET} ${POWER_EN_GPIO}=${POWER_EN_LOGIC}
 fi
 
 # Reset gateway
 if [[ $RESET_GPIO -ne 0 ]]; then
     echo "Concentrator reset through GPIO$RESET_GPIO"
-    if [[ -d /sys/class/gpio/gpio$RESET_GPIO ]]; then
-        echo $RESET_GPIO > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    echo $RESET_GPIO > /sys/class/gpio/export; WAIT_GPIO
-    echo out > /sys/class/gpio/gpio$RESET_GPIO/direction; WAIT_GPIO
-    echo 0 > /sys/class/gpio/gpio$RESET_GPIO/value; WAIT_GPIO
-    echo 1 > /sys/class/gpio/gpio$RESET_GPIO/value; WAIT_GPIO
-    echo 0 > /sys/class/gpio/gpio$RESET_GPIO/value; WAIT_GPIO
-    echo $RESET_GPIO > /sys/class/gpio/unexport; WAIT_GPIO
+    ${GPIOSET} ${RESET_GPIO}=0
+    ${GPIOSET} ${RESET_GPIO}=1
+    ${GPIOSET} ${RESET_GPIO}=0
 fi
 
 exit 0
